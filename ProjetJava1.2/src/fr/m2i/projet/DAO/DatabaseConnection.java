eae5e7c44fc1d25f94ea36ab3c397d4dcece3b24
marie <marierouge@macbook-pro-de-marie.home>
2024-05-03 16:14:38 +0200

    nouvelle version

---
+++ ProjetJava1.2/src/fr/m2i/projet/DAO/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package fr.m2i.projet.DAO;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+	
+	private static String URL = "jdbc:mysql://localhost:3306/GIP?useSSL=false&serverTimezone=UTC";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "root";
+	
+	
+	private static Connection connection;
+	public static Connection getConnection() throws SQLException {
+		if(connection ==null || connection.isClosed()) {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		}
+		//System.out.println("connecté à la base de données !");
+		return connection;
+	}
+}
---
+++ ProjetJava1.2/src/fr/m2i/projet/DAO/MontreDAO.java
@@ -0,0 +1,139 @@
+package fr.m2i.projet.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import fr.m2i.projet.entities.Montre;
+
+public class MontreDAO implements GenericDAO<Montre>{
+	
+	private Connection connection;
+	
+	public MontreDAO() {
+		try {
+			connection = DatabaseConnection.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+	}
+
+	@Override
+	public Montre findById(Integer id) throws SQLException{
+		
+		
+		Statement requete = connection.createStatement();
+		ResultSet montre = requete.executeQuery("SELECT * FROM montre WHERE id ="+id);
+		if(montre != null) {
+			Montre p = new Montre();
+			while(montre.next()) {
+				p.setMarque(montre.getString("marque"));
+				p.setModele(montre.getString("modele"));
+				p.setDiametre(montre.getDouble("diametre"));
+				p.setPrix(montre.getDouble("prix"));
+				p.setDescription(montre.getString("description"));
+		}
+			
+		return p;
+		}
+		return null;
+		
+		
+	}
+
+	@Override
+	public ArrayList<Montre> findAll() throws SQLException {
+		
+		System.out.println("Liste des montres : ");
+		Statement requete = connection.createStatement();
+		ResultSet produits = requete.executeQuery("SELECT * FROM montre");
+		ArrayList<Montre> mesMontres = new ArrayList<>();
+		
+		while(produits.next()) {
+			Montre p = new Montre();
+			p.setMarque(produits.getString("marque"));
+			p.setModele(produits.getString("modele"));
+			p.setDiametre(produits.getDouble("diametre"));
+			p.setPrix(produits.getDouble("prix"));
+			p.setDescription(produits.getString("description"));
+			mesMontres.add(p);
+		}
+		return mesMontres;
+	}
+
+
+
+	@Override
+	public void save(Montre montre, int id){
+		PreparedStatement ps;
+		System.out.println("debug");
+		System.out.println(montre.getModele());
+		
+		if(id ==-1) {
+		
+			try {
+				ps = connection.prepareStatement("INSERT INTO montre(marque, modele, diametre, prix, description) VALUES (?,?,?,?,?)");
+				ps.setString(1, montre.getMarque());
+				ps.setString(2, montre.getModele());
+				ps.setDouble(3, montre.getDiametre());
+				ps.setDouble(4, montre.getPrix());
+				ps.setString(5, montre.getDescription());
+				ps.executeUpdate();
+				System.out.println("Nouveau produit ajouté !");
+				
+			} catch (SQLException e) {
+				System.out.println( e.getSQLState());
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				ps = connection.prepareStatement("UPDATE montre SET marque =?, modele=?, diametre=?, prix=?, description=? WHERE id = ?");
+				ps.setString(1, montre.getMarque());
+				ps.setString(2, montre.getModele());
+				ps.setDouble(3, montre.getDiametre());
+				ps.setDouble(4, montre.getPrix());
+				ps.setString(5, montre.getDescription());
+				ps.setInt(6, id);
+				ps.executeUpdate();
+				System.out.println("Produit modifié !");
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+
+	@Override
+	public void delete(Montre m, int id) {
+
+		try {
+			Statement requete = connection.createStatement();
+			boolean montre = requete.execute("DELETE FROM montre WHERE id ="+id);
+			if(montre == false) {
+				System.out.println("Produit supprimé.");}
+			else {
+				System.out.println("Ce produit n'a pas pu être supprimé.");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+
+
+
+	
+	
+}
